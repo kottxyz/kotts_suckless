a01ec686bd30e28f2f25bb39d341223e658aaca1
kottxyz <160097500+kottxyz@users.noreply.github.com>
2024-06-26 23:54:09 -0500

    Add files via upload

---
+++ slock/config.def.h
@@ -0,0 +1,16 @@
+/* user and group to drop privileges to */
+static const char *user  = "nobody";
+static const char *group = "nobody";
+
+static const char *colorname[NUMCOLS] = {
+	[INIT] =   "black",     /* after initialization */
+	[INPUT] =  "#005577",   /* during input */
+	[FAILED] = "#CC3333",   /* wrong password */
+};
+
+/* treat a cleared input like a wrong password (color) */
+static const int failonclear = 1;
+
+/* Background image path, should be available to the user above */
+/* Set absolute path: /home/username/wallpaper.png */
+static const char* background_image = "";
